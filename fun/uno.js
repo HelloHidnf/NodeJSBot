3160a7f65d5be8b9bf8939509f7d88fa1d64721b
HelloHidnf <HelloHidnf@gmail.com>
2024-02-01 01:52:45 +0000

    having a fun game of uno

--- fun/uno.js
+++ fun/uno.js
@@ -9,17 +9,15 @@ module.exports = {
      * @param {Client} client 
      */
 
-    execute(message, client){
+    execute(message){
         if (!message.content === "uno" && !message.author.id === "410643436044156938") return
 
         embed = new EmbedBuilder()
         .setTitle("Hey")
         .setDescription("Do you want to have [a fun game of uno](https://i.redd.it/pihobmhcl1k51.jpg)?")
-	.setColor(0xffffff)
+	    .setColor(0xffffff)
 
-        if (message.author.id === "410643436044156938"){
-            message.channel.send({ embeds: [embed] })
-            message.delete()
-        }
+        message.channel.send({ embeds: [embed] })
+        message.delete()
     }
 }
