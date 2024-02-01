ed79e1f351f68fb5cb95846431265337a97b3991
HelloHidnf <HelloHidnf@gmail.com>
2024-02-01 01:10:04 +0000

    idk what creator update is but multi-coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -2,7 +2,7 @@ const { Message, EmbedBuilder } = require("discord.js")
 
 module.exports = {
     name: "coinflip",
-    description: "this command does a coinflip, | to seporate the heads and tails",
+    description: "this command does a coinflip, | to seporate the heads and tails, also, if you put a comma followed by a number, you get multiple coinflips",
 
     /**
      * @param {String[]} args
@@ -12,16 +12,41 @@ module.exports = {
     execute(message, _, args){
         message.delete()
 
+        let head = 0
+        let tail = 0
+        let loop = args.join(" ").indexOf(",")
+        let count = NaN
+        if (loop != -1){
+            count = args.join(" ").substring(loop+1, args.join(" ").length)
+        }
+        
+        try{
+            count = parseInt(count)
+        }catch{}
+        if(isNaN(count)) count = 1
+        else if (count > 1000000) return message.reply("but why (cap is 1 million)")
 
+        let tempnum = 0
+        let number = 0
 
-        let tempnum = Math.random() * 1
-        let number = Math.round(tempnum)
+        for (let i = 0; i < count; i++){
+            tempnum = Math.random() * 1
+            number = Math.round(tempnum)
+    
+            if (number === 0) tail++
+            else head++
+        }
+
         let split = args.join(" ").indexOf("|")
+
+        if (loop === -1){
+            loop = `Tails: ${args.join(" ").substring(split+1, args.join(" ").length)}`
+        }else loop = `Tails: ${args.join(" ").substring(split+1, loop)}`
 
         coinbed = new EmbedBuilder()
         .setAuthor({ name: message.author.username, iconURL: message.author.avatarURL() })
         .setColor(0x00ffc8)
-        .setFooter({ text: tempnum.toString() })
+        .setFooter({ text: `${head}|${tail}` })
         .setTimestamp()
         if(args[0] === undefined){
             if (number === 0){
@@ -36,12 +61,12 @@ module.exports = {
         }
         else{
             if (number === 0){
-                coinbed.setTitle(`Tails: ${args.join(" ").substring(split+1, args.join(" ").length)}`)
+                coinbed.setTitle(loop)
                 .setDescription(`Heads: ${args.join(" ").substring(0, split)}`)
                 .setImage("https://cdn.discordapp.com/attachments/988903771650285608/1067894529908297728/tails.png")
             }else{
                 coinbed.setTitle(`Heads: ${args.join(" ").substring(0, split)}`)
-                .setDescription(`Tails: ${args.join(" ").substring(split+1, args.join(" ").length)}`)
+                .setDescription(loop)
                 .setImage("https://cdn.discordapp.com/attachments/988903771650285608/1067894529530806353/heads.png")
             }
         }
--- commands/creator.js
+++ commands/creator.js
@@ -12,7 +12,9 @@ module.exports = {
         embed = new EmbedBuilder()
         .setTitle("Bot Creator")
         .setDescription("The bot creator is <@410643436044156938>, me, I do this for fun and this is my third bot, I am not good at english even though its my first and only language, in other words I'm stupid")
-        .addFields({ name: "Socials", value: "[Twitch](https://www.twitch.tv/hellohidnf)\n[YouTube](https://www.youtube.com/channel/UChMrrmwVdde88CIV4ZH0_rw)\nI barely ever go live or upload so good luck trying to catch a stream" })
+        .addFields(
+            { name: "Socials", value: "[Twitch](https://www.twitch.tv/hellohidnf)\n[YouTube](https://www.youtube.com/channel/UChMrrmwVdde88CIV4ZH0_rw)\nI barely ever go live or upload so good luck trying to catch a stream" }
+        )
         .setFooter({ text: "have fun with the bot ig" })
         .setTimestamp()
         .setColor(0x00ffc8)
